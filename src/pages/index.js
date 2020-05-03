f93fd1306c0f85eb2e5b048a06df7156fec6632a
Jón Hjörtur <jonhjortur@protonmail.com>
2020-05-03 14:35:22 +0000

    More pagination. See myNotes.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,40 +3,54 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
-import { Row, Col } from 'reactstrap'
-import Sidebar from '../components/Sidebar'
+import { Row, Col } from "reactstrap"
+import Sidebar from "../components/Sidebar"
+import PaginationLinks from "../components/paginationLinks"
+import { number } from "prop-types"
 
-const IndexPage = () => (
-  <Layout pageTitle="">
-    <SEO title="Home" keywords={[`/*WHAT TO PUT HERE!!?*/`]} />
+const IndexPage = () => {
+  const postsPerPage = 2
+  let numberOfPages
+  return (
+    <Layout pageTitle="">
+      <SEO title="Home" keywords={[`/*WHAT TO PUT HERE!!?*/`]} />
       <StaticQuery
-      query={indexQuery}
-      render={data => {
-        return (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => (
-              <Post
-              key={node.id}
-                title={node.frontmatter.title}
-                slug={node.fields.slug}
-                author={node.frontmatter.author}
-                body={node.excerpt}
-                date={node.frontmatter.date}
-                fluid={node.frontmatter.image.childImageSharp.fluid}
-                tags={node.frontmatter.tags}
-              />
-            ))}
-          </div>
-        )
-      }}
-    />
-  </Layout>
-)
+        query={indexQuery}
+        render={data => {
+          numberOfPages = Math.ceil(
+            data.allMarkdownRemark.totalCount / postsPerPage
+          )
+          return (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Post
+                  key={node.id}
+                  title={node.frontmatter.title}
+                  slug={node.fields.slug}
+                  author={node.frontmatter.author}
+                  body={node.excerpt}
+                  date={node.frontmatter.date}
+                  fluid={node.frontmatter.image.childImageSharp.fluid}
+                  tags={node.frontmatter.tags}
+                />
+              ))}
+              {/* Þarf þessa gildisveitingu?? */}
+              <PaginationLinks currentPage={1} numberOfPages={numberOfPages} />
+            </div>
+          )
+        }}
+      />
+    </Layout>
+  )
+}
 
 const indexQuery = graphql`
   {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }
-      limit: 2) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 2
+    ) {
+      totalCount
       edges {
         node {
           id
--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -2,10 +2,11 @@ import React from "react"
 import Layout from "../components/layout"
 import Post from "../components/Post"
 import { graphql } from "gatsby"
+import PaginationLinks from '../components/paginationLinks'
 
 const postList = props => {
   const posts = props.data.allMarkdownRemark.edges
-  const { currentPage } = props.pageContext
+  const { currentPage, numberOfPages } = props.pageContext
 
   return (
     <Layout pageTitle={`Page: ${currentPage}`}>
@@ -21,6 +22,8 @@ const postList = props => {
           fluid={node.frontmatter.image.childImageSharp.fluid}
         />
       ))}
+      {/* Þarf þessa gildisveitingu?? */}
+      <PaginationLinks currentPage={currentPage} numberOfPages={numberOfPages}/>
     </Layout>
   )
 }
